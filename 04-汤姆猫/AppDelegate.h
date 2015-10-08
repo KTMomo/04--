79bf62e996a67bb1c144aa4d5160bdbdcd777f37
ckt <ckt@ckts-macbook-pro.local>
2015-10-07 23:35:10 -0700

    Initial Commit

---
+++ 04-汤姆猫/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  04-汤姆猫
+//
+//  Created by ckt on 15/10/7.
+//  Copyright © 2015年 momo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
